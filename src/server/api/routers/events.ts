86c3d511f84663ab2c3c1ea8c1e645a9d8267f8c
Egor Limenko <egor.limenko@gmail.com>
2023-03-26 10:02:02 +0200

    Add rate limiter

--- src/server/api/routers/events.ts
+++ src/server/api/routers/events.ts
@@ -1,10 +1,22 @@
 import { z } from "zod";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+import { TRPCError } from '@trpc/server';
 
 import {
   createTRPCRouter,
   publicProcedure,
   protectedProcedure,
 } from "~/server/api/trpc";
+
+
+// TODO: for testing purpose. Overall this entity does not need this
+// Create a new ratelimiter, that allows 3 requests per 1 seconds
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true
+});
 
 export const eventsRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
@@ -27,6 +39,12 @@ export const eventsRouter = createTRPCRouter({
     const { title } = input;
 
     const authorId = ctx.session.user.id;
+
+    const { success } = await ratelimit.limit(authorId);
+
+    if (!success) {
+      throw new TRPCError({ code: 'TOO_MANY_REQUESTS' });
+    }
 
     const event = await ctx.prisma.event.create({
       data: {
